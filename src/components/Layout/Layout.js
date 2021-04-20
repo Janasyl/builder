6d45c98622df000fc59f75a4c634fa616eb287ef
[Janasyl] <jduishenbae@gmail.com>
2021-04-20 11:09:08 +0600

    add hoc folder and auxiliary

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,12 @@
 import { useState } from "react";
 import Toolbar from "./Toolbar/Toolbar";
 import Drawer from "./Drawer/Drawer";
+import Aux from '../../hoc/Auxiliary.js';
 
 import classes from "./Layout.module.css";
 
 const Layout = ({ children }) => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
-  return (
-    <div className={classes.Layout}>
-      <Toolbar openDrawer={() => setDrawerOpen(true)} />
-      <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
-      <main>
-        {children}
-      </main>
-    </div>
+  
   );
 }
 
