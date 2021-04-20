284e9583d6e71b68d4f65280f823700b845f729c
[Janasyl] <jduishenbae@gmail.com>
2021-04-20 13:54:27 +0600

    d

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import classes from "./Logo.module.css"
-import logo from "../../images/logo.svg"
+// import logo from "../../images/logo.svg"
 const Logo = () => {
     return ( 
         <div className={classes.Logo}>
-            <img  alt="Logo of the Houseplant project" src={logo} className={classes.Logo} />
-            <div>Houseplant</div>
+            {/* <img  alt="Logo of the Bistro project" src={logo} className={classes.Logo} /> */}
+            <div>Bistro</div>
         </div>
      );
 }
