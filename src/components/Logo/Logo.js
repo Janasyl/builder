bc1c3815fcee1b6c20654c04d60082e2770b4c0e
[Janasyl] <jduishenbae@gmail.com>
2021-04-23 13:47:19 +0600

    add logo svg

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import classes from "./Logo.module.css"
-// import logo from "../../images/logo.svg"
+import logo from "../../images/bistro.svg"
 const Logo = () => {
     return ( 
         <div className={classes.Logo}>
-            {/* <img  alt="Logo of the Bistro project" src={logo} className={classes.Logo} /> */}
+            <img  alt="Logo of the Bistro project" src={logo} className={classes.Logo} />
             <div>Bistro</div>
         </div>
      );
