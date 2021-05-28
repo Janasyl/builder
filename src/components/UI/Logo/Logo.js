e21fc9477db43256bb65c465e41b658faa9c62fd
Janasyl Duishenbaev <jduishenbaev@gmail.com>
2021-05-28 15:40:19 +0600

    add logo

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -1,10 +1,10 @@
 import classes from "./Logo.module.css";
-
+import logo from "../../../images/logo2.svg";
 
 const Logo = () => {
   return (
     <div className={classes.Logo}>
-
+     <img src={logo} alt="Logo of the Bistro project" />
       <div>Bistro</div>
     </div>
   );
